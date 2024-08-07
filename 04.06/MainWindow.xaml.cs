4b006f7b9544c0737a144448429ff0222bf598bd
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-07 10:50:56 +0300

    lab/ex-2

--- 04.06/MainWindow.xaml.cs
+++ 04.06/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Threading;
 
 namespace _04._06
@@ -19,35 +20,42 @@ namespace _04._06
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_Tick;
-
-
             timer.Start();
+
+
+            ProcessListView.SelectionChanged += ProcessListView_SelectionChanged;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-
             processes = Process.GetProcesses().ToList();
-
-
             ProcessListView.ItemsSource = processes;
+        }
+
+        private void ProcessListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ProcessListView.SelectedItem is Process selectedProcess)
+            {
+
+                ProcessNameTextBlock.Text = selectedProcess.ProcessName;
+                ProcessIdTextBlock.Text = selectedProcess.Id.ToString();
+                StartTimeTextBlock.Text = selectedProcess.StartTime.ToString();
+                TotalProcessorTimeTextBlock.Text = selectedProcess.TotalProcessorTime.ToString();
+                ThreadsCountTextBlock.Text = selectedProcess.Threads.Count.ToString();
+                InstancesCountTextBlock.Text = Process.GetProcessesByName(selectedProcess.ProcessName).Count().ToString();
+            }
         }
 
         private void UpdateIntervalButton_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-
                 int interval = int.Parse(UpdateIntervalTextBox.Text);
-
-
                 if (interval <= 0)
                 {
                     MessageBox.Show("Інтервал оновлення має бути додатнім числом.", "Помилка");
                     return;
                 }
-
-
                 timer.Interval = TimeSpan.FromSeconds(interval);
             }
             catch (Exception ex)
